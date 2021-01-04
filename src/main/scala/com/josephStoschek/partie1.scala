d2d0b90cc139732c637d54c6bbc154ea0ba04c33
Louise <louise.stoschek@esme.fr>
2021-01-04 11:08:53 +0100

    Q1 avec  valeurs manquantes

--- src/main/scala/com/josephStoschek/partie1.scala
+++ src/main/scala/com/josephStoschek/partie1.scala
@@ -20,33 +20,73 @@ object partie1 {
     val df_rename = df.withColumnRenamed("_c0", "dataframe")
     df_rename.show(30)*/
 
-    //Question 2 : Rajouter une colonne contenant la date du fichier duquel vient chacune des lignes (y'a une fonction dans Spark qui vous le récupère depuis le nom du fichier directement, c'est cadeau).
     import sparkSession.implicits._
-    val df1: DataFrame = sparkSession.read.option("header", false).option("delimiter", ";").option("inferSchema", true).csv("C:\\Users\\stosc\\Documents\\ESME\\Ingé3_2020-2021\\FrameworkBigData\\TP2_note\\src\\main\\scala\\com\\josephStoschek\\data\\02112020.csv")
-    val df1_count = df1.count()
+    var df1: DataFrame = sparkSession.read.option("header", false).option("delimiter", ";").option("inferSchema", true).csv("C:\\Users\\stosc\\Documents\\ESME\\Ingé3_2020-2021\\FrameworkBigData\\TP2_note\\src\\main\\scala\\com\\josephStoschek\\data\\02112020.csv")
+    var df1_count = df1.count()
     while (df1_count <8) {
-      val new_row= Seq(("null")).toDF("dataframe")
-      val new_df1 = df1.union(new_row)
-      println(new_df1)
+      df1 = df1.union(Seq(("null")).toDF)
+      df1_count = df1_count + 1
     }
-    //println(df1)
-    val df2: DataFrame = sparkSession.read.option("header", false).option("delimiter", ";").option("inferSchema", true).csv("C:\\Users\\stosc\\Documents\\ESME\\Ingé3_2020-2021\\FrameworkBigData\\TP2_note\\src\\main\\scala\\com\\josephStoschek\\data\\03112020.csv")
-    val df2_count = df2.count()
-    while (df2_count <8) {
-      val new_row= Seq(("null")).toDF("dataframe")
-      val new_df2 = df2.union(new_row)
-      println(new_df2)
-    }
+    //df1.show()
 
-    /*val df3: DataFrame = sparkSession.read.option("header", false).option("delimiter", ";").option("inferSchema", true).csv("C:\\Users\\stosc\\Documents\\ESME\\Ingé3_2020-2021\\FrameworkBigData\\TP2_note\\src\\main\\scala\\com\\josephStoschek\\data\\04112020.csv")
-    val df4: DataFrame = sparkSession.read.option("header", false).option("delimiter", ";").option("inferSchema", true).csv("C:\\Users\\stosc\\Documents\\ESME\\Ingé3_2020-2021\\FrameworkBigData\\TP2_note\\src\\main\\scala\\com\\josephStoschek\\data\\05112020.csv")
-    val df5: DataFrame = sparkSession.read.option("header", false).option("delimiter", ";").option("inferSchema", true).csv("C:\\Users\\stosc\\Documents\\ESME\\Ingé3_2020-2021\\FrameworkBigData\\TP2_note\\src\\main\\scala\\com\\josephStoschek\\data\\06112020.csv")
-    val df6: DataFrame = sparkSession.read.option("header", false).option("delimiter", ";").option("inferSchema", true).csv("C:\\Users\\stosc\\Documents\\ESME\\Ingé3_2020-2021\\FrameworkBigData\\TP2_note\\src\\main\\scala\\com\\josephStoschek\\data\\07112020.csv")
+    var df2: DataFrame = sparkSession.read.option("header", false).option("delimiter", ";").option("inferSchema", true).csv("C:\\Users\\stosc\\Documents\\ESME\\Ingé3_2020-2021\\FrameworkBigData\\TP2_note\\src\\main\\scala\\com\\josephStoschek\\data\\03112020.csv")
+    var df2_count = df2.count()
+    while (df2_count <8) {
+      df2 = df2.union(Seq(("null")).toDF)
+      df2_count = df2_count + 1
+    }
+    //df2.show()
+
+    var df3: DataFrame = sparkSession.read.option("header", false).option("delimiter", ";").option("inferSchema", true).csv("C:\\Users\\stosc\\Documents\\ESME\\Ingé3_2020-2021\\FrameworkBigData\\TP2_note\\src\\main\\scala\\com\\josephStoschek\\data\\04112020.csv")
+    var df3_count = df3.count()
+    while (df3_count <8) {
+      df3 = df3.union(Seq(("null")).toDF)
+      df3_count = df3_count + 1
+    }
+    //df3.show()
+
+    var df4: DataFrame = sparkSession.read.option("header", false).option("delimiter", ";").option("inferSchema", true).csv("C:\\Users\\stosc\\Documents\\ESME\\Ingé3_2020-2021\\FrameworkBigData\\TP2_note\\src\\main\\scala\\com\\josephStoschek\\data\\05112020.csv")
+    var df4_count = df4.count()
+    while (df4_count <8) {
+      df4 = df4.union(Seq(("null")).toDF)
+      df4_count = df4_count + 1
+    }
+    //df4.show()
+
+    var df5: DataFrame = sparkSession.read.option("header", false).option("delimiter", ";").option("inferSchema", true).csv("C:\\Users\\stosc\\Documents\\ESME\\Ingé3_2020-2021\\FrameworkBigData\\TP2_note\\src\\main\\scala\\com\\josephStoschek\\data\\06112020.csv")
+    var df5_count = df5.count()
+    while (df5_count <8) {
+      df5 = df5.union(Seq(("null")).toDF)
+      df5_count = df5_count + 1
+    }
+    //df5.show()
+
+    var df6: DataFrame = sparkSession.read.option("header", false).option("delimiter", ";").option("inferSchema", true).csv("C:\\Users\\stosc\\Documents\\ESME\\Ingé3_2020-2021\\FrameworkBigData\\TP2_note\\src\\main\\scala\\com\\josephStoschek\\data\\07112020.csv")
+    var df6_count = df6.count()
+    while (df6_count < 8) {
+      df6 = df6.union(Seq(("null")).toDF)
+      df6_count = df6_count + 1
+    }
+    //df6.show()
+
     val df7 = df1.union(df2)
     val df8 = df7.union(df3)
     val df9 = df8.union(df4)
     val df10 = df9.union(df5)
-    val df_final = df10.union(df6) */
+    val df_final = df10.union(df6)
 
-  }
+    df_final.show(48)
+
+
+
+    //Question 2 : Rajouter une colonne contenant la date du fichier duquel vient chacune des lignes (y'a une fonction dans Spark qui vous le récupère depuis le nom du fichier directement, c'est cadeau).
+
+
+    }
+
+
+
+    /* */
+
+
 }
